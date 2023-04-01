10e7d7b3b372c757be86466095fd2211a16227e2
Jose Lima <jalimag0206@gmail.com>
2023-04-01 14:02:32 -0300

    atualizando estilo do segundo input

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ const numB = document.getElementById('n2');
 function validaNumero(){
     if(numB.value > numA.value){
         document.querySelector('.msg-error').style.display = 'none';
+        numB.style.borderBottom = ''
         document.querySelector('.msg-sucess').style.display = 'block';
     }else{
         document.querySelector('.msg-error').style.display = 'block';
+        numB.style.borderBottom = '2px solid red'; 
         document.querySelector('.msg-sucess').style.display = 'none';
     }
 }
