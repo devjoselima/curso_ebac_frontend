d560b7156760b4293a2c920dbe7afd15d34f0a14
Jose Lima <jalimag0206@gmail.com>
2023-05-05 02:05:28 -0300

    add exercicio_es6

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
-function Pessoa(nome, age){
-    this.name;
-    this.age;
-    this.sayHello = function(){
-        console.log(this.name + " diz olá")
-    }
-}
-
-function Programador(cargo, salario, linguagem){
-    this.cargo = cargo;
-    this.salario = salario;
-    this.linguagem = linguagem;
-
-    Pessoa.call(this, nome, age);
-}
-
-function Designer(cargo, salario, ferramentas){
-    this.cargo = cargo;
-    this.salario = salario;
-    this.ferramentas = ferramentas;
-
-    Pessoa.call(this, nome, age);
-}
-
-const programador1 = new Programador('Pedro', 20, 'dev front-end', 3000, 'Html,css e js');
-const programador2 = new Programador('José', 19, 'dev full-stack', 5000, 'Html,css,js,node,phyton');
-const designer = new Designer('José', 25, 'designer grafico', 5000, 'Photoshop');
+const alunos = [
+    {nome: "Jose", nota: 10},
+    {nome: "Duda", nota: 6},
+    {nome: "João", nota: 5},
+  ]
+  
+  function alunosAprovados(){
+    const aprovados = alunos.filter(aluno => aluno.nota >= 6);
+    return aprovados.map(aluno => aluno.nome);
+  }
+  
+  console.log(alunosAprovados())
+  
+  
+  
+  
+  
