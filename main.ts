1560c80b2f3d5844f444f7af9af72fe51eff5b3c
Jose Lima <jalimag0206@gmail.com>
2023-05-15 19:21:39 -0300

    correção exercicio ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-function multiplicacao(n1: number, n2: number){
+function multiplicacao(n1: number, n2: number):number {
     return n1 * n2;
 }
 
-function saudacao(nome: string){
+function saudacao(nome: string): string{
     return 'Ola ' + nome;
 }
